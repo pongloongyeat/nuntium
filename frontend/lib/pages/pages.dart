4d2cb7d530f20460bc77e544ea8cb594608583f5
Pong Loong Yeat <pongloongyeat@gmail.com>
2024-01-28 16:47:09 +0800

    refactor: resolve imports due to folder name change

--- frontend/lib/pages/pages.dart
+++ frontend/lib/pages/pages.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/constants/constants.dart';
+import 'package:frontend/data/constants.dart';
 import 'package:frontend/widgets/widgets.dart';
 
 part 'landing/landing_page.dart';
